2abd59a089b2f9b5155d69e597d3e195fa903cea
renanele10-glitch <renanele10@gmail.com>
2026-06-13 01:53:54 -0300

    Delete chess directory

--- chess/__init__.py
+++
@@ -1,4 +0,0 @@
-from .chess_game import ChessGame, GameState
-from .render import render_board, render_board_bytes
-from .animator import gif_move, gif_promotion, gif_checkmate
-from .chess_view import XadrezCog, setup
