65605938bc52666666f3898cd198c8c5f47a763e
renanele10-glitch <renanele10@gmail.com>
2026-06-15 17:49:23 -0300

    Add files via upload

---
+++ xadrez_engine/__init__.py
@@ -0,0 +1,4 @@
+from .chess_game import ChessGame, GameState
+from .render import render_board, render_board_bytes
+from .animator import gif_move, gif_promotion, gif_checkmate
+from .chess_view import XadrezCog, setup
